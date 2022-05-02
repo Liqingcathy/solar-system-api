497c816bd242ea62dd1327dec8b2786e660a195b
NinaPatrina <ninochca@gmail.com>
2022-05-01 23:12:53 -0700

    Wave 5. Query Params.

--- app/routes.py
+++ app/routes.py
@@ -35,8 +35,13 @@ def create_planet():
 
 @planets_bp.route("", methods=["GET"])
 def get_all_planets():
+    name_query=request.args.get("name")
+    if name_query:
+        planets =Planet.query.filter_by(name=name_query)
+    else:
+        planets=Planet.query.all()
+
     res = []
-    planets=Planet.query.all()
     for planet in planets:
         res.append({
             "id": planet.id,
